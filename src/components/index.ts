4e8491440e31f6a08b79186f0d5652285faba188
tkiddo <justforgit@163.com>
2021-01-19 16:52:40 +0800

    test deploy again

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,11 +2,11 @@
  * @Author: tkiddo
  * @Date: 2021-01-04 10:37:34
  * @LastEditors: tkiddo
- * @LastEditTime: 2021-01-18 14:27:18
+ * @LastEditTime: 2021-01-19 16:52:18
  * @Description:
  */
 
-import * as MyLayout from './Layout';
+import * as MyLayout from './Layout/index';
 import Loader from './Loader';
 import Page from './Page/Page';
 import DropOption from './DropOption';
