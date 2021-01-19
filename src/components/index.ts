f6e982305d6ee3c37eeb25bd9d92876d801e84b2
tkiddo <justforgit@163.com>
2021-01-19 17:03:08 +0800

    test deploy again

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,11 +2,11 @@
  * @Author: tkiddo
  * @Date: 2021-01-04 10:37:34
  * @LastEditors: tkiddo
- * @LastEditTime: 2021-01-19 16:52:18
+ * @LastEditTime: 2021-01-19 16:59:48
  * @Description:
  */
 
-import * as MyLayout from './Layout/index';
+import * as MyLayout from './Layout';
 import Loader from './Loader';
 import Page from './Page/Page';
 import DropOption from './DropOption';
