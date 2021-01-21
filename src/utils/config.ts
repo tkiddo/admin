4d2860bc0b70adf0f28ce8af9421b36e26183053
tkiddo <justforgit@163.com>
2021-01-21 10:58:13 +0800

    update config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,14 +2,14 @@
  * @Author: tkiddo
  * @Date: 2021-01-05 13:53:40
  * @LastEditors: tkiddo
- * @LastEditTime: 2021-01-20 15:54:25
+ * @LastEditTime: 2021-01-21 10:40:47
  * @Description:
  */
 const isDevelopment = process.env.NODE_ENV === 'development';
 export default {
   siteName: 'admin',
   copyright: 'Ant Design Admin  ©2021 tkiddo',
-  logoPath: '/logo.svg',
+  logoPath: isDevelopment ? '/logo.svg' : '/admin/logo.svg',
   apiPrefix: isDevelopment
     ? 'http://localhost:3000'
     : 'https://service-bg1ac25o-1256164626.gz.apigw.tencentcs.com/release',
