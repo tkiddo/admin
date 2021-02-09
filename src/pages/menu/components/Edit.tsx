4076650083766c5601a0233b61b104e28faaefc8
tkiddo <justforgit@163.com>
2021-02-09 15:20:31 +0800

    menu tree

---
+++ src/pages/menu/components/Edit.tsx
@@ -0,0 +1,7 @@
+/*
+ * @Author: tkiddo
+ * @Date: 2021-02-09 14:51:19
+ * @LastEditors: tkiddo
+ * @LastEditTime: 2021-02-09 14:51:19
+ * @Description:
+ */
--- src/pages/menu/components/Tree.tsx
+++ src/pages/menu/components/Tree.tsx
@@ -2,7 +2,7 @@
  * @Author: tkiddo
  * @Date: 2021-02-09 09:08:54
  * @LastEditors: tkiddo
- * @LastEditTime: 2021-02-09 11:03:18
+ * @LastEditTime: 2021-02-09 14:59:32
  * @Description:
  */
 import React, { FC } from 'react';
@@ -12,14 +12,16 @@ import { Tree } from 'antd';
 import { IRoute } from 'common';
 
 import iconMap from '@/utils/iconMap';
-import { EventDataNode } from 'antd/lib/tree';
+import { DataNode, EventDataNode } from 'antd/lib/tree';
+import { PlusOutlined, DeleteFilled } from '@ant-design/icons';
+
+import styles from './Tree.less';
 
 interface ITreeNode {
   title: string;
   key: string;
   icon: React.ReactNode;
   children: ITreeNode[];
-  source: IRoute;
 }
 
 const genTreeData = (tree: IRoute[]): ITreeNode[] => {
@@ -30,7 +32,6 @@ const genTreeData = (tree: IRoute[]): ITreeNode[] => {
       key: id,
       icon: iconMap[icon],
       children: children ? genTreeData(children) : [],
-      source: item,
     };
   });
 };
@@ -46,9 +47,27 @@ const TreeComponent: FC<IProps> = ({ data, onSelect }) => {
     { node }: { node: EventDataNode },
   ) => {
     console.log(selectedKeys, node);
-    onSelect(node.source);
+    onSelect(node);
   };
-  return <Tree treeData={genTreeData(data)} showIcon onSelect={handleSelect} />;
+  const titleRender = (node: DataNode): React.ReactNode => {
+    const { icon, title } = node;
+
+    return (
+      <div className={styles.node}>
+        {icon}
+        <span className={styles.title}>{title}</span>
+        <PlusOutlined className={styles.icon} />
+        <DeleteFilled className={styles.icon} />
+      </div>
+    );
+  };
+  return (
+    <Tree
+      treeData={genTreeData(data)}
+      onSelect={handleSelect}
+      titleRender={titleRender}
+    />
+  );
 };
 
 export default TreeComponent;
